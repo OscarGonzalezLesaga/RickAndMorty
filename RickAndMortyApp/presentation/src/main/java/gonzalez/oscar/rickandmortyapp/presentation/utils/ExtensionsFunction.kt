01580ee02a09da19b31200d28f88c0353d012f5c
Oscar Gonzalez <oscargonles@gmail.com>
2021-05-23 16:12:16 +0200

    delete unused code

--- RickAndMortyApp/presentation/src/main/java/gonzalez/oscar/rickandmortyapp/presentation/utils/ExtensionsFunction.kt
+++ RickAndMortyApp/presentation/src/main/java/gonzalez/oscar/rickandmortyapp/presentation/utils/ExtensionsFunction.kt
@@ -1,14 +1,11 @@
 package gonzalez.oscar.rickandmortyapp.presentation.utils
 
 import android.graphics.drawable.Drawable
-import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.navigation.NavDirections
-import androidx.navigation.fragment.findNavController
 import com.squareup.picasso.Picasso
 import gonzalez.oscar.rickandmortyapp.R
 
@@ -29,12 +26,4 @@ fun View.hide() {
 
 fun Fragment.toast(text: String, duration: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(context, text, duration).show()
-}
-
-fun Fragment.navigate(navigation: NavDirections) {
-    try {
-        findNavController().navigate(navigation)
-    } catch (exception: Exception) {
-        Log.e("Nav:", exception.toString())
-    }
 }
